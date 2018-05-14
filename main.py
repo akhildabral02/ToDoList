3e90370735367df2481e9ef98bd29ccdfaa12817
akhildabral02@gmail.com <akhildabral02@gmail.com>
2018-05-14 10:56:47 +0530

    This is main file for my To DO List Application

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from tkinter import *
 
+
+class Application(Frame):
+
+    def __init__(self, master):
+        Frame.__init__(self,master)
+        self.grid()
+        self.create_widget()
+
+    def create_widget(self):
+        # First Label of Checking the status of our Task
+        self.status = Label(self, text="STATUS")
+        self.status.grid(row=0, column=0, columnspan=2, sticky=W)
+
+        # Second Label for the task Description
+        self.description = Label(self, text="TASK DESCRIPTION")
+        self.description.grid(row=0, column=3, columnspan=3, sticky=W)
+
+
+# Main Programs Begins here
 root = Tk()
 root.title("To Do List")
-root.geometry("500x200")
+root.geometry("250x200")
+APP = Application(root)
 root.mainloop()
