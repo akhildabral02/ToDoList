084b1217d1fbfd5807801812205f29d929176e3f
akhildabral02@gmail.com <akhildabral02@gmail.com>
2018-05-14 14:51:09 +0530

    Basic Windows with Title and Size of 500 x 200

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ from tkinter import *
 class Application(Frame):
 
     def __init__(self, master):
-        Frame.__init__(self,master)
+        Frame.__init__(self, master)
         self.grid()
         self.create_widget()
 
     def create_widget(self):
         # First Label of Checking the status of our Task
-        self.status = Label(self, text="STATUS")
-        self.status.grid(row=0, column=0, columnspan=2, sticky=W)
+        Label(self, text="STATUS", bg="Light Blue", font=("Calibri", 14)).grid(row=0, column=0, columnspan=2, sticky=W)
+        # since we dont need to access these labels anywhere in the progrmas they are just gonna stay there so we are not assigning them to any variable
 
         # Second Label for the task Description
-        self.description = Label(self, text="TASK DESCRIPTION")
-        self.description.grid(row=0, column=3, columnspan=3, sticky=W)
+        Label(self, text="TASK DESCRIPTION", bg="Light Green", font=("Calibri", 14)).grid(row=0, column=2, columnspan=3,sticky=W)
+        # since we dont need to access these labels anywhere in the progrmas they are just gonna stay there so we are not assigning them to any variable
 
+        # creating Checkbox for the Task completion Status
+
+
+        checkvar = IntVar()
+        Checkbutton(text="First Task",variable=checkvar).grid(row=1,column=0, columnspan=2, sticky=W)
 
 # Main Programs Begins here
 root = Tk()
